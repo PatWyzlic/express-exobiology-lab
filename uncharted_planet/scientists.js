09def6ea0e9dec1fd72b6dabdfcb7fe8f79bde11
patwyzlic <patrickwyzlic@gmail.com>
2022-06-02 17:51:52 -0400

    Updating creation

---
+++ uncharted_planet/scientists.js
@@ -0,0 +1,7 @@
+const scientists = 
+    [
+        {Name: 'Xxylox', 
+        Description: 'A spiky armadillo-like creature that wants to snuggle you with its spikes'}
+    ];
+
+    module.exports = scientists;
--- uncharted_planet/server.js
+++ uncharted_planet/server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const app = require('liquid-express-views')(express());
+const scientists = require('./scientists')
 const port = 3000;
 
-app.listen(port, () => {
-    console.log('Port Linked')
-})
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
 
-app.get('/', (req, res) =>{
-    res.send('')
-})
+app.use(express.urlencoded({extended:false}));
 
 app.get('/new', (req, res) =>{
-    res.render('new')
+    res.render('new', {
+        allScientists:scientists
+    })
 })
 
 app.post('/new', (req,res) => {
+    scientists.push(req.body);
+    res.send(scientists);
+    res.render('new')
+})
 
+app.get('/index', (req, res) =>{
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log('Port Linked')
 })
