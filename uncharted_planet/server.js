712c65d0f84fd6bee60b0e32859e0eeebbbc42dd
patwyzlic <patrickwyzlic@gmail.com>
2022-06-02 14:21:20 -0400

    New is rendered

--- uncharted_planet/server.js
+++ uncharted_planet/server.js
@@ -5,3 +5,15 @@ const port = 3000;
 app.listen(port, () => {
     console.log('Port Linked')
 })
+
+app.get('/', (req, res) =>{
+    res.send('')
+})
+
+app.get('/new', (req, res) =>{
+    res.render('new')
+})
+
+app.post('/new', (req,res) => {
+
+})
